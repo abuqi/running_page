794753bb81fd6f0d93cc8fc4415984b6aac9400a
ABUQI <yang.bbq@gmail.com>
2025-11-28 14:24:10 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,21 +16,17 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteUrl: 'https://run.abuqi.online',
+  logo: 'https://avatars.githubusercontent.com/u/2778934?s=60&v=4',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Summary',
-      url: `${getBasePath()}/summary`,
-    },
-    {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://abuqi.github.io/blog/',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://abuqi.github.io/runnint_page/',
     },
   ],
 };
